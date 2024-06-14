3c2a5b3a7fbf8067cadb0bfc89d2e9ac4ed504bb
Sang Virgo <tansang06092004@gmail.com>
2024-06-14 23:53:45 +0700

    14-6-2024

---
+++ LeetCode/14.06.2024.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX 100
+/*
+https://leetcode.com/problems/climbing-stairs/description/
+*/
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        int cnt=0;
+        
+    }
+};
+
+int main() {
+
+return 0;
+}           
