561e67c255ac2a0145e337ed7fe577043b8bf994
Sang Virgo <tansang06092004@gmail.com>
2024-07-16 00:49:17 +0700

    16-7-2024

---
+++ codeforce/15.7.2024/3.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX 100
+/*
+https://www.spoj.com/PTIT/problems/P182PROA
+
+12 4 12 4 1
+c v0 v1 a l
+
+1: 4
+2: 4+4+1
+3: 4+2*4+1
+*/
+int main() {
+
+return 0;
+}
