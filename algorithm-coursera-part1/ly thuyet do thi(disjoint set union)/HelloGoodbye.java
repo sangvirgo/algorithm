ac2e2ae3277a21813aee05fa11a1a034ff00f52d
Sang Virgo <tansang06092004@gmail.com>
2024-07-30 18:03:27 +0700

    30-7-2024

--- algorithm-coursera-part1/ly thuyet do thi(disjoint set union)/HelloGoodbye.java
+++
@@ -1,9 +0,0 @@
-public class HelloGoodbye {
-    public static void main(String[] args) {
-        if (args.length >= 2) {
-            System.out.println("Hello " + args[0] + " and " + args[1] + ".");
-        } else {
-            System.out.println("Please provide at least two names.");
-        }
-    }
-}
