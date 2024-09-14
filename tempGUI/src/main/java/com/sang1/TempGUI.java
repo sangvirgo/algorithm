d31a92b2077dda0db2eecbeb648eed85e7b86532
Sang Virgo <tansang06092004@gmail.com>
2024-09-14 22:17:41 +0700

    14-9-2024

---
+++ tempGUI/src/main/java/com/sang1/TempGUI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.sang1;
+
+/**
+ *
+ * @author soang
+ */
+public class TempGUI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
