4ee99f46a97981c5d23744a55d1ade6e45128b57
Sang <tansang06092004@gmail.com>
2025-08-06 23:57:06 +0700

    6.8.2025

---
+++ LeetCode/3479.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define MAX 100
+const int MOD = 1e9 + 7;
+/*
+
+*/
+
+class Solution {
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        
+    }
+};
+
+int main() {
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+cout.tie(NULL);
+
+
+return 0;
+}
