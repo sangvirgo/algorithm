dfa38203619d3a9943c2c87c766468e96202115c
Sang <tansang06092004@gmail.com>
2025-09-05 23:53:14 +0700

    5.9.2025

---
+++ LeetCode/4.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define MAX 100
+const int MOD = 1e9 + 7;
+/*
+ *
+ */
+
+class Solution {
+public:
+  double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
+    int n = nums1.size();
+    int m = nums2.size();
+    int l = 0, r = (n != 0 && m != 0) ? min(n, m) : max(n, m);
+    while (l <= r) {
+    }
+  }
+};
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  return 0;
+}
